ecbc64fe29669ff6b16148671f78e09794732077
Nour Mostafa <nour.khtabb@gmail.com>
2024-03-25 08:08:57 +0400

    Add files via upload

---
+++ Projects in C/Lab 7 - Blinking Red & Green LEDs Using SysTick Method/Objects/systimer.d
@@ -0,0 +1,9 @@
+./objects/systimer.o: SysTimer.c SysTimer.h stm32l476xx.h \
+  C:\Users\nourk\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Users\nourk\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\nourk\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\nourk\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\cmsis_armclang_m.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\arm_acle.h \
+  C:\Users\nourk\AppData\Local\Arm\Packs\ARM\CMSIS\6.0.0\CMSIS\Core\Include\m-profile\armv7m_mpu.h \
+  C:\Users\nourk\AppData\Local\Arm\Packs\Keil\STM32L4xx_DFP\2.6.2\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
